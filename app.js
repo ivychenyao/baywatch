31f217a6ef8cb5c50fd83818333caabc89565f29
Ivy Chenyao <ivychenyao@ivys-air.coldspring.marian.edu>
2017-06-28 16:10:05 -0400

    Create a flick object with a name and ID

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 // Object with function inside that is called as soon as page loads
 const app = {
     init: function(formSelector) {
+        this.max = 0
         document
             .querySelector(formSelector)
-            .addEventListener('submit', this.handleSubmit)
+            .addEventListener('submit', this.handleSubmit.bind(this)) 
+            // Bind returns a new copy of function set correctly
+            // 'This' will be whatever 'this' is now
     }, // Don't forget comma
 
     // This is a function but still is a property
     handleSubmit: function(ev) { // ev stands for event
         ev.preventDefault()
         const f = ev.target
-        console.log(f.flickName.value)
+        const flick = {
+            id: this.max + 1,
+            name: f.flickName.value,
+        }
+        this.max++
     },
 }
 
