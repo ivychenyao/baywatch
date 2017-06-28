7a04817b74e90c7b2c846583e82f576554e06896
Ivy Chenyao <ivychenyao@ivys-air.coldspring.marian.edu>
2017-06-28 16:00:50 -0400

    Set up submit and flick form

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Yay!')
+// Object with function inside that is called as soon as page loads
+const app = {
+    init: function(formSelector) {
+        document
+            .querySelector(formSelector)
+            .addEventListener('submit', this.handleSubmit)
+    }, // Don't forget comma
+
+    // This is a function but still is a property
+    handleSubmit: function(ev) { // ev stands for event
+        ev.preventDefault()
+        const f = ev.target
+        console.log(f.flickName.value)
+    },
+}
+
+app.init('form#flick-form') // Run as soon as page loads
