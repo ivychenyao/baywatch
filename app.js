909855d7604f454487807d3015ee44cb52db9400
Ivy Chenyao <ivychenyao@ivys-air.coldspring.marian.edu>
2017-06-28 21:43:31 -0400

    Add favorite and remove buttons

--- app.js
+++ app.js
@@ -5,16 +5,21 @@ const app = {
         you add flick to this array. Not just name. ID and all. One-liner */
         this.max = 0
         this.list = document.querySelector(selectors.listSelector)
+        this.favButton = document.querySelector(selectors.favSelector)
         document
             .querySelector(selectors.formSelector)
             .addEventListener('submit', this.handleSubmit.bind(this)) 
             // Bind returns a new copy of function set correctly
             // 'This' will be whatever 'this' is now
+        
     }, // Don't forget comma
 
     renderListItem(flick) {
         const item = document.createElement('li')
         item.textContent = flick.name
+        // let favButton = document.createElement("BUTTON")
+        // favButton.TEXT_NODE = "Favorite"
+        // document.body.appendChild(favButton)
         return item
     },
 
@@ -27,17 +32,50 @@ const app = {
             name: f.flickName.value,
         }
         const listItem = this.renderListItem(flick)
-        this.list.appendChild(listItem)
+        //this.list.appendChild(listItem)
+        
+        
+        // Favorite button
+        let favButton = document.createElement("fav")
+        let text = document.createTextNode("Favorite")
+        favButton.appendChild(text)
+        document.body.appendChild(favButton)
+        favButton.onclick = function() {favorite(listItem)}
+
+        // Remove button
+        let removeButton = document.createElement("rem")
+        let text2 = document.createTextNode("Remove")
+        removeButton.appendChild(text2)
+        document.body.appendChild(removeButton)
+        
 
         // Appends each entered flick to array
         this.flicks.push(flick)
         console.log(this.flicks)
+
+
+        for(let i = 0; i < this.flicks.length; i++) {
+            this.list.appendChild(listItem)
+        }
+
+        // REMOVING
+        // this.flicks.splice(0, 1);
+
         this.max++
     },
+}
+
+function favorite(itemName) {
+    console.log("HI")
+    document.getElementById("fav").style.backgroundColor = "red";
+    console.log("It works")
+   // itemName
 }
 
 // Run as soon as page loads
 app.init({
     formSelector:'form#flick-form',
     listSelector: '#flick-list',
+    favSelector: '#fav',
+    
 }) 
